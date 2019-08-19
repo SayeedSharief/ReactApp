638ef448c463879cf545560edd27860d2bc7f062
SayeedSharief <sayeedsharief11@gmail.com>
2019-08-20 00:52:33 +0530

    Completed chapter 4, Props and State

--- react_app/src/Library.js
+++ react_app/src/Library.js
@@ -1,33 +1,89 @@
 import React, { Component } from 'react'
 // import {render} from 'react-dom'
 
-
-const Book = ({ author, title, pages }) => {
+//function Component    
+const Book = ({ author, title, pages, freeBookmark }) => {
+    // console.log('Inside Book')
     return (
         <div>
             <h1>Author: {author} </h1>
             <p>TItle: {title}</p>
             <p>Pages: {pages}</p>
+            <p>Free Bookmark: {freeBookmark? 'Yes': 'No'}</p>
         </div>
     )
 }
 
+const Hiring = () => <div>
+    <p>Library is hiring. Please visit www.library.com</p>
+</div>
+
+const NotHiring = () => <div><p>Not Hiring. Come back later</p></div>
+
 class Library extends Component {
+    constructor(props) {
+        // super is used to create new instance of the class
+        super(props)
+        this.state = {
+            open: true,
+            freeBookmark: true,
+            hiring: false
+        }
+        // Make this method accessible "this" properties of this class. 
+        // Here state is being accessed
+        this.toggleOpenClosed = this.toggleOpenClosed.bind(this)
+    }
+    toggleOpenClosed(){
+        //when state changes component will be called again to render state changes
+        this.setState(prevState =>({
+            open: !prevState.open,
+            freeBookmark: !this.state.freeBookmark,
+            hiring: !prevState.hiring
+        }))
+        // OR
+        // this.setState ({
+        //     open: !this.state.open
+        // })
+        // console.log('Function')
+    }
     render() {
         console.log('Props to Library: ', this.props)
-        const books = this.props.books
+        const { books } = this.props
+        // OR
+        // const books = this.props.books
         return (
             <div>
-                {books.map(
-                    (book, i) => <Book key={i} 
-                                    author={book.author}
-                                    title={book.title}
-                                    pages = {book.pages} />
-                )}
+                <div>
+                    <h1>Our Library is {this.state.open ? 'open' : 'closed'}</h1>
+                    {this.state.hiring? <Hiring /> : <NotHiring />}
+                    <button onClick={this.toggleOpenClosed}>Toggle</button>
+                </div>
+                <div>
+                    {books.map(
+                        (book, i) => <Book key={i}
+                            author={book.author}
+                            title={book.title}
+                            pages={book.pages} 
+                            freeBookmark={this.state.freeBookmark}/>
+                    )}
+                </div>
             </div>
         )
     }
 }
 
-{/* <Book author="sayeed" title="Welcome to World" pages="203"/> */ }
+// OR
+// Cleaner syntax, Benifits
+// 1. No need to use constructor
+// 2. no need to bind the toggleOpenClosed(), as arrow function does it by default
+// class Library extends Component{
+//     state = {open: true}
+
+//     toggleOpenClosed = () =>{
+//     }
+
+//     render(){
+            // code as above
+//     }
+// }
 export default Library
