3cd2b27cb0f19387de3a0919e464c302158bfcea
SayeedSharief <sayeedsharief11@gmail.com>
2019-08-19 22:06:48 +0530

    Done chapter 3, React Components

--- react_app/src/index.js
+++ react_app/src/index.js
@@ -1,9 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React, { Component } from 'react'
+import { render } from 'react-dom'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+
+const MessageProps = {
+  author: "sayeed",
+  publisher: "alhamd",
+  pagesCount: 200,
+  progress: 150
+}
+
+// ReactDOM.render(
+//   <div style={style}>
+//     <h1>Hello World</h1>
+//     <p>Enjoy the tutorial</p>
+//   </div>
+//   , 
+//   document.getElementById('root'));
+
+// OR 
+
+// Using Components
+class Message extends Component {
+  getPercent = (decimal) => {
+    return (decimal * 100) + '%'
+  }
+  calPercent = (total, goal) => {
+    return this.getPercent(total/goal)
+  }
+  render() {
+    console.log(this.props)
+    //destructuring
+    const { author, publisher, pages, progress } = this.props
+    return (
+      <section>
+        <div>
+          <h1>Welcome {author}</h1>
+        </div>
+        <div>
+          <p>Your Book was published by {publisher} having {pages} pages</p>
+          <p>You have completed {progress} pages out of {pages} pages </p>
+          <p>Your progress is : <strong>{this.calPercent(progress, pages)}</strong></p>
+        </div>
+      </section>
+    )
+  }
+}
+
+render(<Message author={MessageProps.author} publisher={MessageProps.publisher} pages={MessageProps.pagesCount} progress={MessageProps.progress} />, document.getElementById('root'));
